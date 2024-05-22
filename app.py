a6a9147fa8411d93f8208d723a79b7dd854f2f9c
Rodriasd <veronrodrigo98@gmail.com>
2024-05-22 11:29:40 -0300

    added the error handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from requests.exceptions import HTTPError,Timeout,ConnectionError
 
 formatos_aceptados=['jpg', 'png', 'webp']
 
@@ -18,14 +19,19 @@ def captureTags(url):
     results = soup.find_all('img')
 
     for i, img in enumerate(results, start=1):
+        try:
+            response = requests.get(img['src'])
 
-        response = requests.get(img['src'])
-
-        formato = img['src'][-3:]
-        
-        if formato in formatos_aceptados:
-            with open(f'./img/imagen_{str(i)+'.'+formato}', 'wb') as f:
-                f.write(response.content)
-
+            formato = img['src'][-3:]
+            
+            if formato in formatos_aceptados:
+                with open(f'./img/imagen_{str(i)+'.'+formato}', 'wb') as f:
+                    f.write(response.content)
+        except HTTPError as e:
+            print(e)
+        except Timeout as e:
+            print(e)
+        except ConnectionAbortedError as e:
+            print(e)
 
 captureTags("https://cuevana3i.online/pelicula/")
