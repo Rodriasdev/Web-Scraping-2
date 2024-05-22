338f5d4e567582b14133368477c34c6c31670b8e
Rodriasd <veronrodrigo98@gmail.com>
2024-05-22 09:49:20 -0300

    added the function for capture the 'img' tags

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def requestToThePage(url):
     response = requests.get(url)
 
@@ -8,3 +9,40 @@ def requestToThePage(url):
 
 
     return soup
+
+
+def captureTags(url):
+    soup = requestToThePage(url)
+
+    results = soup.find_all('img')
+
+    
+    for img in results:
+        print(img)
+
+
+    # labels={
+
+    # }
+
+    # for a in results:
+
+    #     if a['href'][:5] != 'https':
+    #         page = requestToThePage(url + a['href'])
+
+    #         res = page.find_all(["h1", "p"])
+
+    #         listOfLabels = []
+
+    #         for label in res:
+    #             listOfLabels.append(label)
+            
+    #         labels[url + a['href']] = listOfLabels
+
+
+
+
+
+
+
+print(captureTags("https://cuevana3i.online/pelicula/"))
